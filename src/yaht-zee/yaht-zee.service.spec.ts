15cfd4c5b3346db06f3d76829c10bb37ed3322b7
tour-panurat <tour12033@hotmail.com>
2024-12-19 16:54:17 +0700

    add all function yahtzee !!!

--- src/yaht-zee/yaht-zee.service.spec.ts
+++ src/yaht-zee/yaht-zee.service.spec.ts
@@ -193,4 +193,340 @@ describe('YahtZeeService', () => {
     // assert
     expect(result).toBe(expectedResult);
   });
+
+  it(`ลูกเต๋าที่ออกหน้า [1,1,1,2,3] จะต้องได้ผลลัพธ์ออกมาเป็น 3`, () => {
+    // arrange
+    const input = [1, 1, 1, 2, 3];
+    const expectedResult = 3;
+
+    // action
+    const result = service.sumScoreThreeOfKind(input);
+
+    // assert
+    expect(result).toBe(expectedResult);
+  });
+
+  it(`ลูกเต๋าที่ออกหน้า [5,2,5,5,1] จะต้องได้ผลลัพธ์ออกมาเป็น 15`, () => {
+    // arrange
+    const input = [5, 2, 5, 5, 1];
+    const expectedResult = 15;
+
+    // action
+    const result = service.sumScoreThreeOfKind(input);
+
+    // assert
+    expect(result).toBe(expectedResult);
+  });
+
+  it(`ลูกเต๋าที่ออกหน้า [2,1,4,4,4] จะต้องได้ผลลัพธ์ออกมาเป็น 12`, () => {
+    // arrange
+    const input = [2, 1, 4, 4, 4];
+    const expectedResult = 12;
+
+    // action
+    const result = service.sumScoreThreeOfKind(input);
+
+    // assert
+    expect(result).toBe(expectedResult);
+  });
+
+  it(`ลูกเต๋าที่ออกหน้า [1,1,3,1,1] จะต้องได้ผลลัพธ์ออกมาเป็น 4`, () => {
+    // arrange
+    const input = [1, 1, 3, 1, 1];
+    const expectedResult = 4;
+
+    // action
+    const result = service.sumScoreFourOfKind(input);
+
+    // assert
+    expect(result).toBe(expectedResult);
+  });
+
+  it(`ลูกเต๋าที่ออกหน้า [5,5,5,5,1] จะต้องได้ผลลัพธ์ออกมาเป็น 4`, () => {
+    // arrange
+    const input = [5, 5, 5, 5, 1];
+    const expectedResult = 20;
+
+    // action
+    const result = service.sumScoreFourOfKind(input);
+
+    // assert
+    expect(result).toBe(expectedResult);
+  });
+
+  it(`ลูกเต๋าที่ออกหน้า [2,4,4,4,4] จะต้องได้ผลลัพธ์ออกมาเป็น 16`, () => {
+    // arrange
+    const input = [2, 4, 4, 4, 4];
+    const expectedResult = 16;
+
+    // action
+    const result = service.sumScoreFourOfKind(input);
+
+    // assert
+    expect(result).toBe(expectedResult);
+  });
+
+  it(`ลูกเต๋าที่ออกหน้า [1,1,3,3,3] จะต้องได้ผลลัพธ์ออกมาเป็น 25`, () => {
+    // arrange
+    const input = [1, 1, 3, 3, 3];
+    const expectedResult = 25;
+
+    // action
+    const result = service.scoreFullHouse(input);
+
+    // assert
+    expect(result).toBe(expectedResult);
+  });
+
+  it(`ลูกเต๋าที่ออกหน้า [2,4,4,4,2] จะต้องได้ผลลัพธ์ออกมาเป็น 25`, () => {
+    // arrange
+    const input = [2, 4, 4, 4, 2];
+    const expectedResult = 25;
+
+    // action
+    const result = service.scoreFullHouse(input);
+
+    // assert
+    expect(result).toBe(expectedResult);
+  });
+
+  it(`ลูกเต๋าที่ออกหน้า [6,6,6,3,3] จะต้องได้ผลลัพธ์ออกมาเป็น 25`, () => {
+    // arrange
+    const input = [6, 6, 6, 3, 3];
+    const expectedResult = 25;
+
+    // action
+    const result = service.scoreFullHouse(input);
+
+    // assert
+    expect(result).toBe(expectedResult);
+  });
+
+  it(`ลูกเต๋าที่ออกหน้า [1,2,3,4,6] จะต้องได้ผลลัพธ์ออกมาเป็น 30`, () => {
+    // arrange
+    const input = [1, 2, 3, 4, 6];
+    const expectedResult = 30;
+
+    // action
+    const result = service.scoreSmallStraight(input);
+
+    // assert
+    expect(result).toBe(expectedResult);
+  });
+
+  it(`ลูกเต๋าที่ออกหน้า [6,5,4,3,1] จะต้องได้ผลลัพธ์ออกมาเป็น 30`, () => {
+    // arrange
+    const input = [6, 5, 4, 3, 1];
+    const expectedResult = 30;
+
+    // action
+    const result = service.scoreSmallStraight(input);
+
+    // assert
+    expect(result).toBe(expectedResult);
+  });
+
+  it(`ลูกเต๋าที่ออกหน้า [2,3,4,5,2] จะต้องได้ผลลัพธ์ออกมาเป็น 30`, () => {
+    // arrange
+    const input = [2, 3, 4, 5, 2];
+    const expectedResult = 30;
+
+    // action
+    const result = service.scoreSmallStraight(input);
+
+    // assert
+    expect(result).toBe(expectedResult);
+  });
+
+  it(`ลูกเต๋าที่ออกหน้า [5,4,3,2,2] จะต้องได้ผลลัพธ์ออกมาเป็น 30`, () => {
+    // arrange
+    const input = [5, 4, 3, 2, 2];
+    const expectedResult = 30;
+
+    // action
+    const result = service.scoreSmallStraight(input);
+
+    // assert
+    expect(result).toBe(expectedResult);
+  });
+
+  it(`ลูกเต๋าที่ออกหน้า [1,2,3,4,5] จะต้องได้ผลลัพธ์ออกมาเป็น 30`, () => {
+    // arrange
+    const input = [1, 2, 3, 4, 5];
+    const expectedResult = 40;
+
+    // action
+    const result = service.scoreLargeStraight(input);
+
+    // assert
+    expect(result).toBe(expectedResult);
+  });
+
+  it(`ลูกเต๋าที่ออกหน้า [2,3,4,5,6] จะต้องได้ผลลัพธ์ออกมาเป็น 30`, () => {
+    // arrange
+    const input = [2, 3, 4, 5, 6];
+    const expectedResult = 40;
+
+    // action
+    const result = service.scoreLargeStraight(input);
+
+    // assert
+    expect(result).toBe(expectedResult);
+  });
+
+  it(`ลูกเต๋าที่ออกหน้า [5,4,3,2,1] จะต้องได้ผลลัพธ์ออกมาเป็น 30`, () => {
+    // arrange
+    const input = [5, 4, 3, 2, 1];
+    const expectedResult = 40;
+
+    // action
+    const result = service.scoreLargeStraight(input);
+
+    // assert
+    expect(result).toBe(expectedResult);
+  });
+
+  it(`ลูกเต๋าที่ออกหน้า [5,2,4,3,1] จะต้องได้ผลลัพธ์ออกมาเป็น 30`, () => {
+    // arrange
+    const input = [5, 2, 4, 3, 1];
+    const expectedResult = 40;
+
+    // action
+    const result = service.scoreLargeStraight(input);
+
+    // assert
+    expect(result).toBe(expectedResult);
+  });
+
+  it(`ลูกเต๋าที่ออกหน้า [1,2,3,4,5] จะต้องได้ผลลัพธ์ออกมาเป็น 15`, () => {
+    // arrange
+    const input = [1, 2, 3, 4, 5];
+    const expectedResult = 15;
+
+    // action
+    const result = service.scoreChance(input);
+
+    // assert
+    expect(result).toBe(expectedResult);
+  });
+
+  it(`ลูกเต๋าที่ออกหน้า [1,1,1,2,3] จะต้องได้ผลลัพธ์ออกมาเป็น 8`, () => {
+    // arrange
+    const input = [1, 1, 1, 2, 3];
+    const expectedResult = 8;
+
+    // action
+    const result = service.scoreChance(input);
+
+    // assert
+    expect(result).toBe(expectedResult);
+  });
+
+  it(`ลูกเต๋าที่ออกหน้า [1,1,3,3,3] จะต้องได้ผลลัพธ์ออกมาเป็น 11`, () => {
+    // arrange
+    const input = [1, 1, 3, 3, 3];
+    const expectedResult = 11;
+
+    // action
+    const result = service.scoreChance(input);
+
+    // assert
+    expect(result).toBe(expectedResult);
+  });
+
+  it(`ลูกเต๋าที่ออกหน้า [1,2,3,4,6] จะต้องได้ผลลัพธ์ออกมาเป็น 16`, () => {
+    // arrange
+    const input = [1, 2, 3, 4, 6];
+    const expectedResult = 16;
+
+    // action
+    const result = service.scoreChance(input);
+
+    // assert
+    expect(result).toBe(expectedResult);
+  });
+
+  it(`ลูกเต๋าที่ออกหน้า [5,4,3,2,1] จะต้องได้ผลลัพธ์ออกมาเป็น 15`, () => {
+    // arrange
+    const input = [5, 4, 3, 2, 1];
+    const expectedResult = 15;
+
+    // action
+    const result = service.scoreChance(input);
+
+    // assert
+    expect(result).toBe(expectedResult);
+  });
+
+  it(`ลูกเต๋าที่ออกหน้า [1,1,1,1,1] จะต้องได้ผลลัพธ์ออกมาเป็น 50`, () => {
+    // arrange
+    const input = [1, 1, 1, 1, 1];
+    const expectedResult = 50;
+
+    // action
+    const result = service.scoreYAHTZEE(input);
+
+    // assert
+    expect(result).toBe(expectedResult);
+  });
+
+  it(`ลูกเต๋าที่ออกหน้า [2,2,2,2,2] จะต้องได้ผลลัพธ์ออกมาเป็น 50`, () => {
+    // arrange
+    const input = [2, 2, 2, 2, 2];
+    const expectedResult = 50;
+
+    // action
+    const result = service.scoreYAHTZEE(input);
+
+    // assert
+    expect(result).toBe(expectedResult);
+  });
+
+  it(`ลูกเต๋าที่ออกหน้า [3,3,3,3,3] จะต้องได้ผลลัพธ์ออกมาเป็น 50`, () => {
+    // arrange
+    const input = [3, 3, 3, 3, 3];
+    const expectedResult = 50;
+
+    // action
+    const result = service.scoreYAHTZEE(input);
+
+    // assert
+    expect(result).toBe(expectedResult);
+  });
+
+  it(`ลูกเต๋าที่ออกหน้า [4,4,4,4,4] จะต้องได้ผลลัพธ์ออกมาเป็น 50`, () => {
+    // arrange
+    const input = [4, 4, 4, 4, 4];
+    const expectedResult = 50;
+
+    // action
+    const result = service.scoreYAHTZEE(input);
+
+    // assert
+    expect(result).toBe(expectedResult);
+  });
+
+  it(`ลูกเต๋าที่ออกหน้า [5,5,5,5,5] จะต้องได้ผลลัพธ์ออกมาเป็น 50`, () => {
+    // arrange
+    const input = [5, 5, 5, 5, 5];
+    const expectedResult = 50;
+
+    // action
+    const result = service.scoreYAHTZEE(input);
+
+    // assert
+    expect(result).toBe(expectedResult);
+  });
+
+  it(`ลูกเต๋าที่ออกหน้า [6,6,6,6,6] จะต้องได้ผลลัพธ์ออกมาเป็น 50`, () => {
+    // arrange
+    const input = [6, 6, 6, 6, 6];
+    const expectedResult = 50;
+
+    // action
+    const result = service.scoreYAHTZEE(input);
+
+    // assert
+    expect(result).toBe(expectedResult);
+  });
 });
--- src/yaht-zee/yaht-zee.service.ts
+++ src/yaht-zee/yaht-zee.service.ts
@@ -3,8 +3,150 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class YahtZeeService {
   sumByTarget(input: number[], target: number): number {
+    if (input.length !== 5) return 0;
+
+    if (target > 6 || target < 1) return 0;
+
     if (!Array.isArray(input)) return 0;
 
-    return input.filter((item) => item === target).length * target;
+    return (
+      input.filter((item) => {
+        if (item > 6 || item < 1) return 0;
+        return item === target;
+      }).length * target
+    );
+  }
+  sumScoreThreeOfKind(input: number[]): number {
+    let result = 0;
+    for (let i = 0; i < input.length; i++) {
+      let count = 0;
+      for (let j = 0; j < input.length; j++) {
+        if (input[i] === input[j]) {
+          count++;
+        }
+
+        if (count === 3) {
+          result = input[i] * 3;
+        }
+      }
+    }
+    return result;
+  }
+
+  sumScoreFourOfKind(input: number[]): number {
+    let result = 0;
+    for (let i = 0; i < input.length; i++) {
+      let count = 0;
+      for (let j = 0; j < input.length; j++) {
+        if (input[i] === input[j]) {
+          count++;
+        }
+
+        if (count === 4) {
+          result = input[i] * 4;
+        }
+      }
+    }
+    return result;
+  }
+
+  scoreFullHouse(input: number[]): number {
+    const target = [1, 2, 3, 4, 5, 6];
+    let twoDuplicateNumber = false;
+    let threeDuplicateNumber = false;
+    for (let i = 0; i < target.length; i++) {
+      const check = input.filter((item) => item === target[i]);
+      if (check.length === 2) twoDuplicateNumber = true;
+      if (check.length === 3) threeDuplicateNumber = true;
+    }
+    return twoDuplicateNumber && threeDuplicateNumber ? 25 : 0;
+  }
+
+  scoreSmallStraight(input: number[]): number {
+    const sortNumber = input.sort((a, b) => a - b);
+    console.log(sortNumber);
+    // เรียงหน้า
+    if (
+      sortNumber[0] === 1 &&
+      sortNumber[1] === 2 &&
+      sortNumber[2] === 3 &&
+      sortNumber[3] === 4
+    ) {
+      return 30;
+    }
+    if (
+      sortNumber[0] === 2 &&
+      sortNumber[1] === 3 &&
+      sortNumber[2] === 4 &&
+      sortNumber[3] === 5
+    ) {
+      return 30;
+    }
+
+    if (
+      sortNumber[0] === 3 &&
+      sortNumber[1] === 4 &&
+      sortNumber[2] === 5 &&
+      sortNumber[3] === 6
+    ) {
+      return 30;
+    }
+
+    // เรียงหลัง
+    if (
+      sortNumber[1] === 2 &&
+      sortNumber[2] === 3 &&
+      sortNumber[3] === 4 &&
+      sortNumber[4] === 5
+    ) {
+      return 30;
+    }
+
+    if (
+      sortNumber[1] === 3 &&
+      sortNumber[2] === 4 &&
+      sortNumber[3] === 5 &&
+      sortNumber[4] === 6
+    ) {
+      return 30;
+    }
+
+    return 0;
+  }
+
+  scoreLargeStraight(input: number[]): number {
+    const sortNumber = input.sort((a, b) => a - b);
+    if (
+      sortNumber[0] === 1 &&
+      sortNumber[1] === 2 &&
+      sortNumber[2] === 3 &&
+      sortNumber[3] === 4 &&
+      sortNumber[4] === 5
+    ) {
+      return 40;
+    }
+    if (
+      sortNumber[0] === 2 &&
+      sortNumber[1] === 3 &&
+      sortNumber[2] === 4 &&
+      sortNumber[3] === 5 &&
+      sortNumber[4] === 6
+    ) {
+      return 40;
+    }
+    return 0;
+  }
+
+  scoreChance(input: number[]): number {
+    return input.reduce((a, b) => a + b);
+  }
+
+  scoreYAHTZEE(input: number[]): number {
+    const target = input[0];
+    const checkDuplicateNumber = input.filter((item) => item === target).length;
+    if (checkDuplicateNumber === 5) {
+      return 50;
+    }
+    return 0;
   }
 }
